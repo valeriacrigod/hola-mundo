4035451d6ed8f7b668fa564320582819fc6c55ce
valeriacrigod <127272667+valeriacrigod@users.noreply.github.com>
2023-04-21 12:34:18 -0300

    index.php
    Modifiqué la información de los elementos h1, h2 y agregé dos elementos p con información de La computadora IBM 1401.

--- index.php
+++ index.php
@@ -7,7 +7,20 @@
   <script src="script.js"></script>
 </head>
 <body>
-  <h1>Bienvenidos Hola Mundo</h1>
-  <h2>ChoicesPracticas Profesionalizantes</2>
+  <h1>IBM 1401</h1>
+  
+  <h2>Primera Computadora Comercial</h2>
+  
+  <p>El sistema IBM 1401 se introdujo para proporcionar funciones
+   de sistema a gran escala a los usuarios de computadoras de pequeñas
+    empresas que necesitaban más flexibilidad del sistema para 
+    resolver sus problemas más pequeños pero únicos.</p> 
+    
+    <p>La computadora 1401 trajo a estas industrias una máquina con una 
+    variedad de beneficios: lectura y perforación de tarjetas de alta 
+    velocidad; impresión de alta velocidad; uso de cinta magnética para 
+    entrada, salida y almacenamiento adicional; programa almacenado de 
+    instrucciones; capacidad aritmética y lógica.</p>
+  
 </body>
 </html>
